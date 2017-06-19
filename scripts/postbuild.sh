25088081929555ef0edab329ec92ea2a8235779c
Manoj Gautam <manoj.gautam@olenepal.org>
2017-06-19 10:51:42 +0545

    scripts refactored

---
+++ scripts/postbuild.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# Author: Manoj
+# Purpose: post build configuration
+# Email: <manoj.gautam@olenepal.org>
+
+#TODO: redefine.
+/dspace/bin/dspace index-discovery -fb
+# Re indexing
+/dspace/bin/dspace index-discovery -b -s
+
+echo "Building non-SOLR search and browse indexes... Please wait"
+
